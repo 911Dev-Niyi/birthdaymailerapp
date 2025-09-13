1c89149bf23e9ba63360ff05fa029898d0f2c822
911Dev-Niyi <anjorinadeniyi811@gmail.com>
2025-09-13 23:23:31 +0100

    Fix for error 404 /user:1

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-export const addUser = (data) => axios.post(`${BACKEND_URL}/user`, data);
-export const  testEmail = (email) => axios.post(`${BACKEND_URL}/user/test`, { email });
-export const unsubscribeUser = (email) => axios.delete(`${BACKEND_URL}/user/unsubscribe`, { data: { email } });
+export const addUser = (data) => axios.post(`${BACKEND_URL}api/user`, data);
+export const  testEmail = (email) => axios.post(`${BACKEND_URL}api/user/test`, { email });
+export const unsubscribeUser = (email) => axios.delete(`${BACKEND_URL}api/user/unsubscribe`, { data: { email } });

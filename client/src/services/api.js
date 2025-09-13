deaf56c59314d77a8fb0cd49ce418dcc2fb9eec3
911Dev-Niyi <anjorinadeniyi811@gmail.com>
2025-09-14 00:39:50 +0100

    Edited api.js to include "/"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 // updated to /api/user
-export const addUser = (data) => axios.post(`${BACKEND_URL}api/user`, data);
-export const  testEmail = (email) => axios.post(`${BACKEND_URL}api/user/test`, { email });
-export const unsubscribeUser = (email) => axios.delete(`${BACKEND_URL}api/user/unsubscribe`, { data: { email } });
+export const addUser = (data) => axios.post(`${BACKEND_URL}/api/user`, data);
+export const  testEmail = (email) => axios.post(`${BACKEND_URL}/api/user/test`, { email });
+export const unsubscribeUser = (email) => axios.delete(`${BACKEND_URL}/api/user/unsubscribe`, { data: { email } });

70da6176617ac1f76eed4c58e2fac43e00200c1a
911Dev-Niyi <anjorinadeniyi811@gmail.com>
2025-09-22 17:20:28 +0100

    Update sendBirthdayEmail.js
    commented nodemailer import

--- utils/sendBirthdayEmail.js
+++ utils/sendBirthdayEmail.js
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -32,5 +32,6 @@ const sendBirthdayEmail = async (user) => {
         }
     }
 };
+
 
 export default sendBirthdayEmail;
